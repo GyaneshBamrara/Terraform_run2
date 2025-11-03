c4da23ee4d17311667a5dcf58eb4dbc82d8660dc
GyaneshBamrara <112647673+GyaneshBamrara@users.noreply.github.com>
2025-11-03 15:14:03 +0530

    Update c2-generic-variables.tf

--- c2-generic-variables.tf
+++ c2-generic-variables.tf
@@ -9,7 +9,7 @@ variable "aws_region" {
 variable "environment" {
   description = "Environment Variable used as a prefix"
   type = string
-  default = "dev"
+  default = "stag"
 }
 # Business Division
 variable "business_divsion" {
